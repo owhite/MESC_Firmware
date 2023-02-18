a87cf8229713185a272fc6c9528056bb6a31c149
Owen White <smouldering.dog@gmail.com>
2023-02-17 20:17:32 -0500

    70V

--- MESC_F405RG/Core/Src/main.c
+++ MESC_F405RG/Core/Src/main.c
@@ -24,6 +24,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+/* OWEN */
 #include "MESCbat.h"
 #include "MESCflash.h"
 #include "MESCprofile.h"
